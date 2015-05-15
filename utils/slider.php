69fffb8df00d567307e827c2c601349cfd68efe6
ruben <ee10195@fe.up.pt>
2015-05-15 10:37:23 +0100

    slider actualizado e esquema de pastas alterado (utils com header, slider e futuros ficheiros semelhantes

---
+++ utils/slider.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Car Rental</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="animated_jumbo/engine/style.css" />
+    <script type="text/javascript" src="animated_jumbo/engine/jquery.js"></script>
+    <!-- End WOWSlider.com HEAD section -->
+
+</head>
+
+
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+    <div class="ws_images"><ul>
+            <li><img src="data1/images/9193_1280x800.jpg" alt="9193_1280x800" title="9193_1280x800" id="wows1_0"/></li>
+            <li><img src="data1/images/bmw4grancoupecaranddesertlandscapewebheader.jpg" alt="bmw-4-gran-coupe-car-and-desert-landscape-web-header" title="bmw-4-gran-coupe-car-and-desert-landscape-web-header" id="wows1_1"/></li>
+            <li><img src="data1/images/folio_aut07.jpg" alt="folio_AUT07" title="folio_AUT07" id="wows1_2"/></li>
+            <li><a href="http://wowslider.com"><img src="data1/images/mp412csupercarwhitecarlandscapeeveningphotobackground.jpg" alt="slider responsive" title="MP4-12C-supercar-white-car-landscape-evening-photo-background" id="wows1_3"/></a></li>
+            <li><img src="data1/images/folio_aut062.jpg" alt="folio_AUT06-2" title="folio_AUT06-2" id="wows1_4"/></li>
+        </ul></div>
+    <div class="ws_bullets"><div>
+            <a href="#" title="9193_1280x800"><span><img src="data1/tooltips/9193_1280x800.jpg" alt="9193_1280x800"/>1</span></a>
+            <a href="#" title="bmw-4-gran-coupe-car-and-desert-landscape-web-header"><span><img src="data1/tooltips/bmw4grancoupecaranddesertlandscapewebheader.jpg" alt="bmw-4-gran-coupe-car-and-desert-landscape-web-header"/>2</span></a>
+            <a href="#" title="folio_AUT07"><span><img src="data1/tooltips/folio_aut07.jpg" alt="folio_AUT07"/>3</span></a>
+            <a href="#" title="MP4-12C-supercar-white-car-landscape-evening-photo-background"><span><img src="data1/tooltips/mp412csupercarwhitecarlandscapeeveningphotobackground.jpg" alt="MP4-12C-supercar-white-car-landscape-evening-photo-background"/>4</span></a>
+            <a href="#" title="folio_AUT06-2"><span><img src="data1/tooltips/folio_aut062.jpg" alt="folio_AUT06-2"/>5</span></a>
+        </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">jquery photo gallery</a> by WOWSlider.com v8.0</div>
+    <div class="ws_shadow"></div>
+</div>
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
