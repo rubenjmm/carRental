50be7d52734a3a1decf671889af3ff12c0b5107f
ruben <ee10195@fe.up.pt>
2015-05-31 18:31:44 +0100

    (...)

--- registar.php
+++ registar.php
@@ -6,16 +6,17 @@ require ROOT."utils/navbar.php";
 ?>
 
 <body>
-<div class="row" style="z-index: -1; position: fixed;right: ">
+<div class="row fullscreen" style="z-index: -1; position: fixed;">
     <?php
         require ROOT."utils/slider.php";
     ?>
 </div>
 <div class="row" style="z-index: 0; position: relative; right: -25px;">
-    <h2>Registo</h2>
+    <h2></h2>
 </div>
 <div class="row" style="z-index: 1;  position: relative; top: -25px;">
     <div class="col-md-6">
+        <!--
         <form method="post" class="form-style-10">
             <h2>Condutor</h2>
             <p><input type="text" name="nome" placeholder="Nome"/></p>
@@ -24,34 +25,58 @@ require ROOT."utils/navbar.php";
             <p><input type="text" name="ncartaconducao" placeholder="Nº Carta de Condução"/> </p>
             <p align="right"><input type="submit" value="Registar" /> </p>
         </form>
+        -->
     </div>
-    <div class="col-md-6">
-        <form method="post" class="form-style-10" action="registar_validar.php">
-            <h2>Cliente</h2>
-            <div class="row">
+    <div class="col-lg-6">
+        <form method="post" class="form-style-10" style="background: #fa7921;" action="registar_validar.php" method="post">
+            <h2>Registo</h2>
+            <p><div class="row">
                 <div class="col-md-9 left">
-                    <input type="text" name="nome" placeholder="Nome">
+                    <input type="text" style="border:1px solid #03238d" name="nome" placeholder="Nome">
                 </div>
-                <div class="col-md-3 right">
-                    <select name="tipo" >
+                <div class="col-md-3 right" >
+                    <select name="tipo" style="border:1px solid #03238d">
                         <option value="Sr.">Sr.</option>
                         <option value="Sra.">Sra.</option>
                         <option value="Empresa">Co.</option>
                     </select>
                 </div>
-            </div>
-            <p><input placeholder="Password" type="password" name="password"/> </p>
-            <p><input placeholder="Username" type="text" name="username"/> </p>
-            <p><input type="text" name="Morada" placeholder="Morada"/> </p>
-            <p><input type="text" name="nif" placeholder="nif"/> </p>
-            <p><input type="text" name="telefone" placeholder="Telefone"/> </p>
-            <p><input type="text" name="telemovel" placeholder="Telemovel"/> </p>
-            <p><input type="text" name="email" placeholder="e-mail"/> </p>
+            </div></p>
+            <p><input placeholder="Password" type="password" style="border:1px solid #03238d" name="password"/> </p>
+            <p><input placeholder="Username" type="text" style="border:1px solid #03238d" name="username"/> </p>
+            <p><input type="text" style="border:1px solid #03238d" name="morada" placeholder="Morada"/> </p>
+            <p><input type="text" style="border:1px solid #03238d" name="nif" placeholder="nif"/> </p>
+            <p><input type="text" style="border:1px solid #03238d" name="telefone" placeholder="Telefone"/> </p>
+            <p><input type="text" style="border:1px solid #03238d" name="telemovel" placeholder="Telemovel"/> </p>
+            <p><input type="text" style="border:1px solid #03238d" name="email" placeholder="e-mail"/> </p>
             <!--<p><text class="h4">Condutor? </text><input type="radio" name="condutor" value="sim" style="position: relative; left: 10px;"><text class="h5" style="position: relative; left: 15px;"> Sim </text><input type="radio" name="condutor" value="nao" style="position: relative; left: 50px;"><text class="h5" style="position: relative; left: 55px"> Não</text></p>-->
             <p align="right"><input type="submit" value="Registar" /> </p>
         </form>
     </div>
 </div>
+<?php
+    if (isset($_GET['erro']) and !(empty($_GET['erro'])))
+    {
+        ?>
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-2" style="position:relative; top: -250px;">
+                <?php
+                    if ($_GET['erro']== 1):
+                    {
+                        ?>
+                        <p><text style="color: #fc0000; font-size: 200%; font-weight: bold">Erro: Username já registado!</text></p>
+                        <p><text style="color: #fc0000; font-size: 200%; font-weight: bold">Tente um username diferente</text></p>
+                        <?php
+                    }
+                    elseif ($_GET['erro']== 2):
+                    {
+                        ?>
+                        <text style="color: #fc0000; font-size: 200%; font-weight: bold">Erro: Registo incompleto!</text>
+                        <?php
+                    }endif;
+    }
+?>
+            </div>
+        </div>
 </body>
-
 </html>
--- registar_validar.php
+++ registar_validar.php
@@ -2,42 +2,79 @@
 defined('ROOT') or define ('ROOT', (dirname(__FILE__))."/");
 require "utils/head.php";
 require_once "classes/empresa.php";
+require_once "classes/cliente.php";
 ?>
 
 <?php
-    if(isset($_POST['nome']) and isset($_POST['password']) and isset($_POST['username']) and isset($_POST['Morada']) and isset($_POST['nif']) and isset($_POST['telefone']) and isset($_POST['telemovel']) and isset($_POST['email']))
+    if(isset($_POST['nome']) and isset($_POST['password']) and isset($_POST['username']) and isset($_POST['morada']) and isset($_POST['telemovel']) and isset($_POST['email']) and !( empty($_POST['nome'])) and !( empty($_POST['password'])) and !( empty($_POST['username'])) and !( empty($_POST['morada'])) and !( empty($_POST['telemovel'])) and !( empty($_POST['email']))):
     {
-        /*if (isset($_POST['condutor']))
+        if (($_POST['tipo'] == "Sr.") || ($_POST['tipo'] == "Sra.")):
         {
+            $allCli = Cliente::getAllUsernames();
+            $allCliLowCase = array_map('strtolower', $allCli);
+            if (in_array(strtolower($_POST['username']), $allCliLowCase)) {
+                unset($_POST);
+                header("Location: registar.php?erro=1");
+                exit;
+            }
+
+            //__construct($id = null, $nome = null, $titulo = null, $morada = null, $username = null, $password = null, $telemovel=null, $email=null) {
+            $cliente= new Cliente(NULL, $_POST['nome'], $_POST['tipo'] ,$_POST['morada'], $_POST['username'], md5($_POST['password']), $_POST['telemovel'], $_POST['email']);
+            $cliente->insert();
+
+            unset($_POST);
+            $ok = 1;
         }
-        else
+        elseif (($_POST['tipo'] == "Empresa") and isset($_POST['telefone']) and isset($_POST['nif']) and !(empty($_POST['telefone'])) and !(empty($_POST['nif']))):
         {
-        }*/
+            $allCo = Empresa::getAllUsernames();
+            $allCoLowCase = array_map('strtolower', $allCo);
 
-        $allCo = Empresa::getAll();
-        $allCoLowCase = array_map('strtolower', $allCo);
+            if (in_array(strtolower($_POST['username']), $allCoLowCase)) {
+                unset($_POST);
+                header("Location: registar.php?erro=1");
+                exit;
+            }
 
-        if (in_array(strtolower($_POST['username']), $allCoLowCase)) {
-            header('registar.php?erro=1');
-        }
+            //__construct($id = null, $nome = null, $morada = null, $username=null, $password=null, $nif=null, $telefone=null, $telemovel=null, $email=null) {
+            $company = new Empresa(NULL, $_POST['nome'], $_POST['morada'], $_POST['username'], md5($_POST['password']), $_POST['nif'], $_POST['telefone'], $_POST['telemovel'], $_POST['email']);
+            $company->insert();
 
-        //__construct($id = null, $nome = null, $morada = null, $username=null, $password=null, $nif=null, $telefone=null, $telemovel=null, $email=null) {
-        $company = new Empresa(NULL, $_POST['nome'], $_POST['morada'], $_POST['password'], $_POST['morada'], $_POST['cidade'], $_POST['telefone'], $_POST['telemovel'], $_POST['email'], $_SESSION['empresa_id']);
-        $company->insert();
+            unset($_POST);
+            $ok = 1;
+        }endif;
     }
+    else:
+    {
+        unset($_POST);
+        header("Location: registar.php?erro=2");
+        exit;
+    }
+    endif;
 
-    else
-        header('registar.php?erro=2');
+    unset($_POST);
+
+    if ($ok == 1):
+    {
+        require ROOT."utils/navbar.php";
+        ?>
+            <meta http-equiv="refresh" content="5; url=index.php" />
+
+            <body>
+                <div class="row fullscreen" style="z-index: -1; position: fixed;">
+                    <?php
+                        require ROOT."utils/slider.php";
+                    ?>
+                </div>
+                <div class="row">
+                    <text style="font-size: 250%" align="center">Registo Completo</text><br>
+                    <text style="font-size: 150%" align="center">Sera redirecionado para a pagina inicila dentro de 5 segundos</text>
+                    <a href="index.php">(clique aqui se não for redirecionado)</a>
+                </div>
+            </body>
+            </html>
+            <?php
+        $ok = 0;
+    } endif;
+
 ?>
-
-<?php
-    require ROOT."utils/navbar.php";
-?>
-
-<meta http-equiv="refresh" content="5;url=http://www.google.com/" />
-<body>
-<div class="row">
-    <text style="position: relative; top: 150px; font-size: 250%" align="center">Registo Completo</text><br>
-    <text style="position: relative; top: 150px; font-size: 150%" align="center">Sera redirecionado para a pagina inicila dentro de 5 segundos</text>
-</div>
-</body>
